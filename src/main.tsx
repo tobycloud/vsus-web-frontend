a273f0a62ce8049c0e1e6f6f16a93b8ea8d70725
Toby Cm <newtobycm@gmail.com>
2024-01-25 05:34:17 +0000

    for real this time

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { render } from 'preact'
+import { App } from './app.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+render(<App />, document.getElementById('app')!)
