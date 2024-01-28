681e99af328860238e55e780f4efe157bc73a139
pdt1806 <nmphuc1806@gmail.com>
2024-01-28 15:06:12 -0800

    refactor: :recycle:

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,46 +6,26 @@ const pocketbase = new PocketBase(Constants.PocketBaseURL);
 export default pocketbase;
 
 export async function userSignIn(email: string, password: string) {
-  try {
-    pocketbase.authStore.clear();
-    await pocketbase.collection("users").authWithPassword(email, password);
-  } catch (error) {
-    throw error;
-  }
+  pocketbase.authStore.clear();
+  await pocketbase.collection("users").authWithPassword(email, password);
 }
 
 export async function userSignInOAuth2(provider: string) {
-  try {
-    pocketbase.authStore.clear();
-    await pocketbase.collection("users").authWithOAuth2({ provider });
-  } catch (error) {
-    throw error;
-  }
+  pocketbase.authStore.clear();
+  await pocketbase.collection("users").authWithOAuth2({ provider });
 }
 
 export async function userSignUp(data: Object) {
-  try {
-    pocketbase.authStore.clear();
-    await pocketbase.collection("users").create(data);
-  } catch (error) {
-    throw error;
-  }
+  pocketbase.authStore.clear();
+  await pocketbase.collection("users").create(data);
 }
 
 export async function getImageURL(user: User) {
-  try {
-    return await pocketbase.getFileUrl(user, user.avatar);
-  } catch (error) {
-    throw error;
-  }
+  return await pocketbase.getFileUrl(user, user.avatar);
 }
 
 export async function getWorkspaces(user: User) {
-  try {
-    return await pocketbase.collection("workspaces").getFullList({
-      filter: `owner = "${user.id}"`,
-    });
-  } catch (error) {
-    throw error;
-  }
+  return await pocketbase.collection("workspaces").getFullList({
+    filter: `owner = "${user.id}"`,
+  });
 }
